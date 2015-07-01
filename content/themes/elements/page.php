74ba0bee31496037d4036bd3411b924185c5ce71
Luc Awater <lucawater@gmail.com>
2015-07-01 15:19:24 +0200

    basic dom setup

--- content/themes/elements/page.php
+++ content/themes/elements/page.php
@@ -1,28 +1,40 @@
 <?php
 get_header();
+?>
 
-// Loop into ACF groups
-if( have_rows('page') ): $i_anchor = 1; $i_par = 0;
-  while( have_rows('page') ): the_row();
-    
-    // Hero section is placed before main
-    
-    if( get_row_layout() == 'text' ):
-      include( locate_template('content/text.php') ); $i_anchor++;
-    elseif( get_row_layout() == 'image' ):
-      include( locate_template('content/image.php') );
-    elseif( get_row_layout() == 'grid_primary' ):
-      include( locate_template('content/gridPri.php') ); $i_anchor++;
-    elseif( get_row_layout() == 'grid_secondary' ):
-      include( locate_template('content/gridSec.php') ); $i_anchor++;
-    elseif( get_row_layout() == 'slider' ):
-      include( locate_template('content/slider.php') ); $i_anchor++;
-    elseif( get_row_layout() == 'parallax' ): $i_par++;
-      include( locate_template('content/parallax.php') );
-    endif;
-    
-  endwhile;
-endif;
+<aside>
+  <?php
+  // Left background image(s)
+  $left_images = get_field( 'left_images' );
+
+  echo '<div class="aside-images">';
+
+  foreach( $left_images as $image ):
+    echo '<li><img src="' . $image['sizes']['large'] . '"></li>';
+  endforeach;
+
+  echo '</div>';
+
+  // Left text
+  $left_text = get_field( 'left_text' );
+
+  echo '<div class="aside-text">' . $left_text . '</div>';
+  ?>
+</aside>
+
+<main role="main">
+  <?php include( 'includes/nav.php' );
+
+  // Loop into ACF groups
+  if( have_rows('page') ): $i_anchor = 1;
+    while( have_rows('page') ): the_row();
+
+      if( get_row_layout() == 'text' ):
+        include( locate_template('content/text.php') ); $i_anchor++;
+      endif;
+
+    endwhile;
+  endif;
 
 get_footer();
 ?>
