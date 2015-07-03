f21a372448417c865a342ecbc8c983fa26196521
Luc Awater <lucawater@gmail.com>
2015-07-03 10:55:15 +0200

    add basic styling for playlist

--- content/themes/elements/content/playlist.php
+++ content/themes/elements/content/playlist.php
@@ -2,7 +2,7 @@
 echo '<section class="playlist">';
   if( have_rows('playlist_item') ):
 
-    echo '</ul>';
+    echo '<ul>';
 
     while( have_rows('playlist_item') ): the_row();
       $title = get_sub_field( 'playlist_item_title' );
@@ -14,7 +14,7 @@ echo '<section class="playlist">';
 
     endwhile;
 
-    echo '<ul>';
+    echo '</ul>';
 
   endif;
 echo '</section>';
