ffc02b999c3e78a076c494780ff87241a8221f85
Luc Awater <lucawater@gmail.com>
2015-07-03 11:36:35 +0200

    add numbers to playlist items

--- content/themes/elements/content/playlist.php
+++ content/themes/elements/content/playlist.php
@@ -1,17 +1,20 @@
 <?php
-echo '<section class="playlist">';
+echo '<section class="playlist" id="playlist">';
   if( have_rows('playlist_item') ):
 
     echo '<ul>';
+    $i = 1;
 
     while( have_rows('playlist_item') ): the_row();
       $title = get_sub_field( 'playlist_item_title' );
 
       echo
       '<li>
-      <p>' . $title . '<p>
+      <p>0' . $i . '. ' . $title . '<p>
       </li>';
 
+      $i++;
+      
     endwhile;
 
     echo '</ul>';
