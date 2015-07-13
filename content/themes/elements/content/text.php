00c48f56f092343cafa815eea2c7ef39d08f5a16
Luc Awater <lucawater@gmail.com>
2015-07-13 07:13:22 +0200

    add text block bg option

--- content/themes/elements/content/text.php
+++ content/themes/elements/content/text.php
@@ -4,10 +4,11 @@ $title = get_sub_field( 'text_title' );
 $content = get_sub_field( 'text_content' );
 $button_text = get_sub_field( 'text_button_text' );
 $button_link = get_sub_field( 'text_button_link' );
+$background = get_sub_field( 'text_o_bg' );
 
 //Output
 echo
-'<section class="text has-anchor" id="anchor-' . $i_anchor . '">
+'<section class="text has-anchor' . ( ($background) ? ' has-bg' : "") . '" id="anchor-' . $i_anchor . '">
   <div class="section-body">
     ' . ( ($title) ? '<h2 class="s-4 columns is-uppercase is-bold is-aligned-center">' . $title . '</h2>' : "") . '
     ' . $content . '
