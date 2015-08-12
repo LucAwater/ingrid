887b5c5f13814318684c2d0e2792bfd61225809d
Luc Awater <lucawater@gmail.com>
2015-08-12 10:49:08 +0200

    Add skrollr effects  to actrice grid

---
+++ content/themes/elements/coffee/init-skrollr.coffee
@@ -0,0 +1,7 @@
+$ ->
+  # Init Skrollr
+  s = skrollr.init(render: (data) ->
+    #Debugging - Log the current scroll position.
+    #console.log(data.curTop);
+    return
+  )
--- content/themes/elements/page-actrice.php
+++ content/themes/elements/page-actrice.php
@@ -18,23 +18,38 @@ get_header();
   ?>
 
   <ul>
-    <li class="width-40 left-20">
+    <li
+    class="width-40 left-20 skrollable"
+    data-bottom-top="transform: translate3d(0px,100px,0px);"
+    data-top-bottom="transform: translate3d(0px,0px,0px);">
       <img src="<?php echo $image1['sizes']['large']; ?>" width="<?php echo $image1['width']; ?>" height="<?php echo $image1['height']; ?>">
     </li>
 
-    <li class="width-30 left-50 top-30">
+    <li
+    class="width-30 left-50 top-30"
+    data-bottom-top="transform: translate3d(0px,0px,0px);"
+    data-top-bottom="transform: translate3d(0px,-300px,0px);">
       <img src="<?php echo $image2['sizes']['large']; ?>" width="<?php echo $image2['width']; ?>" height="<?php echo $image2['height']; ?>">
     </li>
 
-    <li class="width-80 left-10">
+    <li
+    class="width-80 left-10"
+    data-bottom-top="transform: translate3d(0px,-100px,0px);"
+    data-top-bottom="transform: translate3d(0px,0,0px);">
       <img src="<?php echo $image3['sizes']['large']; ?>" width="<?php echo $image3['width']; ?>" height="<?php echo $image3['height']; ?>">
     </li>
 
-    <li class="width-40 left-60">
+    <li
+    class="width-40 left-60"
+    data-bottom-top="transform: translate3d(0px,150px,0px);"
+    data-top-bottom="transform: translate3d(0px,-300,0px);">
       <img src="<?php echo $image4['sizes']['large']; ?>" width="<?php echo $image4['width']; ?>" height="<?php echo $image4['height']; ?>">
     </li>
 
-    <li class="width-60 left-20 top-20">
+    <li
+    class="width-60 left-20 top-20"
+    data-bottom-top="transform: translate3d(0px,-200px,0px);"
+    data-top-bottom="transform: translate3d(0px,-300px,0px);">
       <img src="<?php echo $image5['sizes']['large']; ?>" width="<?php echo $image5['width']; ?>" height="<?php echo $image5['height']; ?>">
     </li>
   </ul>
